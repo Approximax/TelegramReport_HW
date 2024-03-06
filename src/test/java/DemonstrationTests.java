7da670376b05d63eb1cc5d50e99277321ad1ec86
Vladislav Kryukov <vlad_pw_v@list.ru>
2024-03-07 00:21:30 +0300

    delete unused buttons class renamed package added logic for secret password transfer

--- src/test/java/DemonstrationTests.java
+++ src/test/java/DemonstrationTests.java
@@ -1,12 +1,20 @@
+import config.credentials.AuthCredentials;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import pages.components.AuthorizationPage;
-import pages.components.MainPage;
-import pages.components.VacanciesPage;
+import pages.AuthorizationPage;
+import pages.MainPage;
+import pages.VacanciesPage;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
@@ -61,7 +69,12 @@ public class DemonstrationTests extends TestBase {
     @Severity(SeverityLevel.BLOCKER)
     @DisplayName("Проверка наличия возможности авторизации через сторонние ресурсы")
     @Tag("login")
-    void authorizationWithPassword() {
+    void authorizationWithPassword() throws IOException {
+
+        AuthCredentials config = ConfigFactory.create(AuthCredentials.class, System.getProperties());
+        String content = "login=secret-login\npassword=secret-pass";
+        Path propsPath = Paths.get("/tmp/secret.properties");
+        Files.write(propsPath, content.getBytes(StandardCharsets.UTF_8));
 
         step("Открываем сайт", () -> {
             open("https://spb.hh.ru/");
@@ -72,8 +85,8 @@ public class DemonstrationTests extends TestBase {
         });
 
         step("Вводим данные для авторизации", () -> {
-            authorizationPage.setLogin()
-                    .setPassword();
+            authorizationPage.setLogin(config.login())
+                    .setPassword(config.password());
         });
 
         step("Проверяем, что авторизация прошла успешно", () -> {
